120bd222e2a624385d901fd5d278c9f8f151a369
Vikram Bodicherla <vikrambd@yahoo-inc.com>
2015-02-08 22:51:11 -0500

    Removed some error code #3

--- app/src/main/java/com/markiv/images/ui/GImageSearchAdapter.java
+++ app/src/main/java/com/markiv/images/ui/GImageSearchAdapter.java
@@ -36,6 +36,8 @@ class GImageSearchAdapter extends BaseAdapter {
 
     private final SearchActivity.ViewFlipperManager mViewSwitcherManager;
 
+    private boolean mFirstImageLoaded = false;
+
     public GImageSearchAdapter(Context context, SearchSession searchSession,
             GISService.GISImageViewFactory imageViewFactory,
             SearchActivity.ViewFlipperManager viewSwitcherManager) {
@@ -168,28 +170,22 @@ class GImageSearchAdapter extends BaseAdapter {
         public void setData(final GISImageView view, SearchSession.Result data) {
             if (view != null) {
                 if (data != null) {
-                    view.setBitmapLoadedListener(new GISImageView.BitmapLoadedListener() {
-                        @Override
-                        public void onBitmapLoaded() {
-                            view.post(new Runnable() {
-                                @Override
-                                public void run() {
-                                    mViewSwitcherManager.showGrid();
-                                }
-                            });
-                        }
-                    });
+                    if (!mFirstImageLoaded) {
+                        view.setBitmapLoadedListener(new GISImageView.BitmapLoadedListener() {
+                            @Override
+                            public void onBitmapLoaded() {
+                                mFirstImageLoaded = true;
+                                mViewSwitcherManager.showGrid();
+                            }
+                        });
+                    }
                     view.setGISResult(data);
-
-                    // TODO I'd want to do this when the first imageview is set, but for some reason
-                    // that
-                    // TODO doesnt work
-                    //mViewSwitcherManager.showGrid();
                 } else if (mSearchSession.getResultCount() == 0) {
                     mViewSwitcherManager.showMessage(String.format(mContext.getResources()
                             .getString(R.string.no_search_results), mSearchSession.getQuery()));
                 }
                 else {
+                    mSearchSession.kill();
                     mViewSwitcherManager.showError(mError);
                 }
             }
